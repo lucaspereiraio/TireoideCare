9c6f06bb2050821f3c1c7c881921b2e637047cea
alessandraMurat <alessandra.murat@outlook.com>
2024-10-09 18:26:53 -0300

    Estilização da tela de cadastrar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
+"use client";
+
 import React from "react";
-import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div className="container w-full flex items-center justify-center gap-8">
-      <Link href="/login">
-        <button>Login</button>
-      </Link>
-      <Link href="/register">
-        <button>Cadastro</button>
-      </Link>
+    <div className="container-full h-screen w-full flex items-center justify-center flex-col">
+      <Image
+        src="/images/logo.png"
+        alt="Logo TireoideCare"
+        width={398}
+        height={249}
+      />
+      <p className="text-center text-[14px] text-neuters-gray-70 my-16">
+        Monitore, cuide, viva melhor. <br />
+        Juntos pela sua tiróide.
+      </p>
+
+      <div className="flex flex-col gap-y-4">
+        <button
+          className="rounded-xl w-40 h-11 bg-primary-100 text-neuters-gray-20 font-semibold"
+          onClick={() => router.push("/register")}
+        >
+          Cadastrar
+        </button>
+        <button
+          className="rounded-xl w-40 h-11 bg-primary-20 text-neuters-gray-70 font-semibold"
+          onClick={() => router.push("/login")}
+        >
+          Acessar
+        </button>
+      </div>
     </div>
   );
 }
--- components/Register.tsx
+++ components/Register.tsx
@@ -1,36 +1,85 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Register = () => {
+  const router = useRouter();
   return (
-    <section>
-      <h1>Cadastro</h1>
-      <form>
-        <div>
+    <div className="container-full min-h-screen w-full flex items-center justify-center flex-col px-4">
+      <Image
+        src="/images/logo.png"
+        alt="Logo TireoideCare"
+        width={398}
+        height={249}
+      />
+
+      <h1 className="text-center text-xl text-neuters-gray-70 my-4">
+        Cadastrar
+      </h1>
+      <form className=" w-full md:w-1/4 flex flex-col items-center gap-y-4">
+        {/* <div className="w-full flex flex-col gap-y-2">
           <label htmlFor="name">Nome</label>
-          <input id="name" type="name" placeholder="Digite seu nome" />
-        </div>
-        <div>
+          <input className="border border-neuters-gray-70 border-opacity-50 rounded-lg h-11 px-4 focus:outline-none focus:border-primary-100 focus:ring-1 focus:ring-primary-100" id="name" type="name" placeholder="Digite seu nome" />
+        </div> */}
+        <div className="w-full flex flex-col gap-y-2">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" placeholder="Digite seu email" />
-        </div>
-        <div>
-          <label htmlFor="password">Senha</label>
-          <input id="password" type="password" placeholder="Digite sua senha" />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirme a sua senha</label>
           <input
+            className="border border-neuters-gray-70 border-opacity-50 rounded-lg h-11 px-4 focus:outline-none focus:border-primary-100 focus:ring-1 focus:ring-primary-100"
+            id="email"
+            type="email"
+            placeholder="Digite seu email"
+          />
+        </div>
+        <div className="w-full flex flex-col gap-y-2">
+          <label htmlFor="password">Senha</label>
+          <input
+            className="border border-neuters-gray-70 border-opacity-50 rounded-lg h-11 px-4 focus:outline-none focus:border-primary-100 focus:ring-1 focus:ring-primary-100"
+            id="password"
+            type="password"
+            placeholder="Digite sua senha"
+          />
+        </div>
+        {/* <div className="w-full flex flex-col gap-y-2">
+          <label htmlFor="confirmPassword">Confirme a sua senha</label>
+          <input className="border border-neuters-gray-70 border-opacity-50 rounded-lg h-11 px-4 focus:outline-none focus:border-primary-100 focus:ring-1 focus:ring-primary-100"
             id="confirmPassword"
             type="password"
             placeholder="Confirme a sua senha"
           />
+        </div> */}
+        <div className="h-[1.5px] w-64 bg-neuters-gray-70 bg-opacity-50 mt-6 mb-2"></div>
+        <div className="flex gap-x-2">
+          <Image
+            src="/images/facebook.png"
+            alt="Facebbok"
+            width={24}
+            height={24}
+          />
+          <Image
+            src="/images/instagram.png"
+            alt="Instagram"
+            width={24}
+            height={24}
+          />
+          <Image src="/images/google.png" alt="Google" width={24} height={24} />
         </div>
-        <button>Cadastrar</button>
+
+        <button
+          className="rounded-xl w-40 h-11 bg-primary-100 text-neuters-gray-20 my-4 font-semibold"
+          // onClick={() => router.push("/")}
+        >
+          Entrar
+        </button>
       </form>
       <p>
-        Já tem uma conta? <a href="/login">Cadastre-se</a>
+        Já tem uma conta?{" "}
+        <a className="text-primary-100 font-medium" href="/login">
+          Acessar
+        </a>
       </p>
-    </section>
+    </div>
   );
 };
 
