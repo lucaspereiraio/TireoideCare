9ea9f9956b017c94c5b4b72a0c3a15187854edee
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-10-09 14:21:56 -0300

    form-states
    - Created the form states;
    - Implemented the values on inputs;

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {}
+  );
+
   return (
     <section>
       <h1>Login</h1>
       <form>
         <div>
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" placeholder="Digite seu email" />
+          <input
+            id="email"
+            type="email"
+            placeholder="Digite seu email"
+            value={formData.email}
+          />
         </div>
         <div>
           <label htmlFor="password">Senha</label>
-          <input id="password" type="password" placeholder="Digite sua senha" />
+          <input
+            id="password"
+            type="password"
+            placeholder="Digite sua senha"
+            value={formData.password}
+          />
         </div>
         <a href="/recovery">Esqueceu sua senha?</a>
-        <button>Login</button>
+        <button type="submit">Login</button>
       </form>
       <p>
         Ainda não tem uma conta? <a href="/register">Cadastre-se</a>
--- components/Register.tsx
+++ components/Register.tsx
@@ -1,21 +1,44 @@
 import React from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState<{
+    name?: string;
+    email?: string;
+    password?: string;
+    confirmPassword?: string;
+  }>({});
+
   return (
     <section>
       <h1>Cadastro</h1>
       <form>
         <div>
           <label htmlFor="name">Nome</label>
-          <input id="name" type="name" placeholder="Digite seu nome" />
+          <input
+            id="name"
+            type="name"
+            placeholder="Digite seu nome"
+            value={formData.name}
+          />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" placeholder="Digite seu email" />
+          <input
+            id="email"
+            type="email"
+            placeholder="Digite seu email"
+            value={formData.email}
+          />
         </div>
         <div>
           <label htmlFor="password">Senha</label>
-          <input id="password" type="password" placeholder="Digite sua senha" />
+          <input
+            id="password"
+            type="password"
+            placeholder="Digite sua senha"
+            value={formData.password}
+          />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirme a sua senha</label>
@@ -23,6 +46,7 @@ const Register = () => {
             id="confirmPassword"
             type="password"
             placeholder="Confirme a sua senha"
+            value={formData.confirmPassword}
           />
         </div>
         <button>Cadastrar</button>
